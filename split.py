ebd4f734a6977ccfa9aa1bed886ff1f49b8c3074
Ian Gao <30684063+ig505gi@users.noreply.github.com>
2021-07-01 23:35:20 +0800

    Update split.py

--- split.py
+++ split.py
@@ -1,1 +1,3 @@
 import cv2
+# 2.5 根据横线特征旋转 https://stackoverflow.com/questions/53854066/pythonhow-to-rotate-an-image-so-that-a-feature-becomes-vertical python rotate picture to make a line horizontal 
+# 2.6 水平和垂直投影，找到每一个表格 https://zhuanlan.zhihu.com/p/30391661 
